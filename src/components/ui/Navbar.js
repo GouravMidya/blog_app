ffc0242525026b5f09cddc912beaa6fa8ab748ca
GouravMidya <gouravgmidya@gmail.com>
2024-12-06 00:40:57 +0530

    Added navbar file and imported it into layout file

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export const NavBar = () => {
+  return (
+    <nav className="bg-blue-500 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-lg font-bold">Blog App</h1>
+        <div className="flex space-x-4">
+          <Link href="/">
+            Home
+          </Link>
+          <Link href="/about">
+            About
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};

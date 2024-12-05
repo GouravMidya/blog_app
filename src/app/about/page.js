8b5c25eba0041708d9285385cb59f6edd10ed59f
GouravMidya <gouravgmidya@gmail.com>
2024-12-06 00:32:01 +0530

    minor changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,8 +5,8 @@ export default function About() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-6">About Page</h1>
-      <Button onClick={() => alert("Should send user back to homepage!")}>
-        Go Back
+      <Button onClick={() => alert("Welcome to about us page!")}>
+        About us
       </Button>
     </div>
   );
